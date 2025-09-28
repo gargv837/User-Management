c6982d554a86d644dfc8282876f216fdf45441cb
Vaibhav Garg <gargv837@gmail.com>
2025-09-28 15:00:11 +0530

    [FIX] Modified DB connection call

--- backend/db.js
+++ backend/db.js
@@ -4,12 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export default pool;
